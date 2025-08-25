f5a2802773b91cf5897e673f68313a542b497744
Administrador-Santolin <admin@santolinseguros.com.br>
2025-08-25 12:37:28 -0300

    refatoração para Inicio e dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,34 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { authGuard } from './auth-guard';
+import { Relatorios } from './relatorios/relatorios';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 
-  { path: 'dashboard', 
+  {
+    path: 'dashboard',
     canActivate: [authGuard],
     loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
-   },
+  },
+  {
+    path: 'clientes',
+    canActivate: [authGuard],
+    loadChildren: () => import('./clientes/clientes.routes').then(m => m.CLIENTES_ROUTES)
+  },
+  {
+    path: 'apolices',
+    canActivate: [authGuard],
+    loadChildren: () => import('./apolices/apolices.routes').then(m => m.APOLICES_ROUTES)
+  },
+  { path: 'relatorios', canActivate: [authGuard],component: Relatorios },
+  {
+    path: 'multi-calculo',
+    canActivate: [authGuard],
+    loadChildren: () => import('./multi-calculo/multi-calculo.routes').then(m => m.MULTI_CALCULO_ROUTES)
+  },
 
-  // Rota curinga para qualquer URL não encontrada (redireciona para o login)
-  { path: '**', redirectTo: '/login' }
+  // Rota curinga para qualquer URL não encontrada (redireciona para o dashboard)
+  { path: '**', redirectTo: '/dashboard' }
 ];
